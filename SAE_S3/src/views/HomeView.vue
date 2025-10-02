eddf316f3c53eafc094b9aada24d23a6db99f1da
EthanRTZ <rietz.ethan@gmail.com>
2025-10-02 14:09:52 +0200

    cusstom home view

--- SAE_S3/src/views/HomeView.vue
+++ SAE_S3/src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <h1>Welcome to the Home Page</h1>
-    <p>This is the main landing page of the application.</p>
+    <h1>Golden coast</h1>
   </div>
 </template>
 
@@ -10,3 +9,21 @@ export default {
   name: 'HomeView',
 };
 </script>
+
+<style scoped>
+.home {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #0d47a1;
+  text-align: center;
+}
+
+.home h1 {
+  color: #ffd700;
+  font-size: 3rem;
+  font-weight: 700;
+  margin: 0;
+}
+</style>
